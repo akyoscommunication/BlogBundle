78dccae0a56ab3a7ee27748d3b651f9ffc67e332
Thomas SEBERT <thomas.sebert.akyos@gmail.com>
2022-08-31 15:49:49 +0200

    Fix for php 8.1 and SF 5.4

--- AkyosBlogBundle.php
+++ AkyosBlogBundle.php
@@ -3,11 +3,12 @@
 namespace Akyos\BlogBundle;
 
 use Akyos\BlogBundle\DependencyInjection\BlogBundleExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AkyosBlogBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new BlogBundleExtension();
